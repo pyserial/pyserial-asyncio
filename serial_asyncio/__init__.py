pyserial/pyserial-asyncio

commit fc5ef0ec39a4cca7901439c01f3f4405a1da9fba

Provide serial object via get_extra_info() (closes #62) (#65)

serial_asyncio/__init__.py
@@ -75,8 +75,20 @@ class SerialTransport(asyncio.Transport):
 
     @property
     def serial(self):
-        """The underlying Serial instance."""
+        """The underlying Serial instance.
+
+        Equivalent to get_extra_info("serial")
+        """
         return self._serial
+
+    def get_extra_info(name, default=None):
+        """Get optional transport information.
+
+        Currently only "serial" is available.
+        """
+        if name == "serial":
+            return self._serial
+        return default
 
     def __repr__(self):
         return '{self.__class__.__name__}({self.loop}, {self._protocol}, {self.serial})'.format(self=self)

commit bfb8504111108e57083c203e59b7717c6da7bb16

Fix missing self parameter in get_extra_info
Fixes a bug introduced in #65

serial_asyncio/__init__.py
@@ -81,7 +81,7 @@ class SerialTransport(asyncio.Transport):
         """
         return self._serial
 
-    def get_extra_info(name, default=None):
+    def get_extra_info(self, name, default=None):
         """Get optional transport information.
 
         Currently only "serial" is available.